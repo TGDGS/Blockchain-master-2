1b18af1ebb8d0d2eb0b06b7723c76b64487bfa81
TGDGS <111734730+TGDGS@users.noreply.github.com>
2023-11-09 10:49:40 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+function Generate_Hash () {
+	
+}
+led.stopAnimation()
+basic.clearScreen()
+radio.setGroup(58)
+radio.setTransmitPower(7)
+serial.redirectToUSB()
+let Blocks = 0
+let Block_Reward = 10
+basic.forever(function () {
+	
+})
