1bdd90266be0f863134fc49c2b829ef24b95ad0a
TGDGS <111734730+TGDGS@users.noreply.github.com>
2023-11-09 14:51:44 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ enum RadioMessage {
     message1 = 49434,
     Device_ID = 52974
 }
+datalogger.onLogFull(function () {
+    basic.showString("EF")
+})
 function Generate_Hash () {
     let Block_Hash_Array: number[] = []
     Block_Hash = Math.round(randint(6588, 8973) * (randint(8, 6587) / randint(6.9, 420)))
@@ -12,12 +15,24 @@ function Generate_Hash () {
 input.onButtonPressed(Button.A, function () {
     Generate_Hash()
 })
+function Wallet_Amount () {
+    Wallet = datalogger.createCV("Block_Hash", Value)
+    if (Wallet.split("_") == "") {
+    	
+    }
+    Value += 1
+}
 input.onButtonPressed(Button.AB, function () {
     datalogger.deleteLog(datalogger.DeleteType.Full)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(0)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     serial.writeLine("Hi!")
 })
+let Value = 0
+let Wallet: datalogger.ColumnValue = null
 let Block_Hash = 0
 let ID = 0
 led.stopAnimation()
@@ -30,7 +45,7 @@ let Block_Reward = 10
 ID = 6
 timeanddate.set24HourTime(12, 0, 0)
 datalogger.setColumnTitles("Block_Hash")
-datalogger.mirrorToSerial(true)
+datalogger.mirrorToSerial(false)
 serial.writeLine("Online")
 basic.forever(function () {
 	
